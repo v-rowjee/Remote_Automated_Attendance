b61b066d75beddaabdba41089d97a34a96bcfb36
giresh <gireshgowreesunkur@gmail.com>
2022-06-14 01:04:01 +0400

    login frame completed

--- src/Views/LoginView.java
+++ src/Views/LoginView.java
@@ -1,6 +1,7 @@
 package Views;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class LoginView {
@@ -11,16 +12,24 @@ public class LoginView {
 
     public LoginView(String title){
         frame = new JFrame(title);
-        frame.setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.Y_AXIS));
+        frame.setLayout(new GridLayout(1,2,0,0));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+//        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        Icon icon =new ImageIcon( new ImageIcon(getClass().getResource("thank_you_teacher.png")).getImage().getScaledInstance(400, 400, Image.SCALE_DEFAULT));
+        Icon icon =new ImageIcon( new ImageIcon(getClass().getResource("../images/thank_you_teacher.png")).getImage().getScaledInstance(400, 400, Image.SCALE_DEFAULT));
         JLabel logo = new JLabel(icon);
         logo.setBounds(0, 0, 400, 400);
 
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(6,1));
+        panel.setBorder(new EmptyBorder(50,50,50,50));
+        lblUsername = new JLabel("Username");
+        lblPassword = new JLabel("Password");
+        txtUsername = new JTextField();
+        txtPassword = new JPasswordField();
         btnLogin = new JButton("Login");
+        chxShowPassword = new JCheckBox("Show Password");
 
        // frame.add(btnLogin);
         frame.add(logo);
@@ -32,5 +41,29 @@ public class LoginView {
 
     public JButton getBtnLogin() {
         return btnLogin;
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public JLabel getLblUsername() {
+        return lblUsername;
+    }
+
+    public JLabel getLblPassword() {
+        return lblPassword;
+    }
+
+    public JTextField getTxtUsername() {
+        return txtUsername;
+    }
+
+    public JPasswordField getTxtPassword() {
+        return txtPassword;
+    }
+
+    public JCheckBox getChxShowPassword() {
+        return chxShowPassword;
     }
 }
