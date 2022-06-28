964f3eaed44509eb41e034bc163892b82badbedf
v-rowjee <vedrowjee@gmail.com>
2022-06-28 22:13:09 +0400

    padding dan passwordfield + aastextfield

--- src/Components/AASTextField.java
+++ src/Components/AASTextField.java
@@ -6,16 +6,25 @@ public class AASTextField extends JTextField{
 	public AASTextField(){
 		setBackground(Color.decode("#eeeeee"));
 		setBorder(null);
+		setBorder(BorderFactory.createCompoundBorder(
+				getBorder(),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
 	};
 	public AASTextField(String text){
 		super(text);
 		setBackground(Color.decode("#eeeeee"));
 		setBorder(null);
+		setBorder(BorderFactory.createCompoundBorder(
+				getBorder(),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
 	};
 	public AASTextField(String text, String color){
 		super(text);
 		setBackground(Color.decode(color));
 		setBorder(null);
+		setBorder(BorderFactory.createCompoundBorder(
+				getBorder(),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
 	};
 	public void setBackColor(String color) {
 		setBackground(Color.decode(color));
